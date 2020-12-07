142eb9ea44c6240f7eafeecbf69ea5bb2d9a4f52
Dmitry <56539475+username8t3@users.noreply.github.com>
2020-12-07 18:02:03 +0300

    :airplane:

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,69 +1,82 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
-    if ( props.users.length === 0 ) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',
-                followed: true,
-                fullName: 'Dmitry',
-                status: 'Learning',
-                location: {
-                    country: 'Russia',
-                    city: 'St. Petersburg',
-                }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',            followed: false,
-                fullName: 'Alexey',
-                status: 'Drinking',
-                location: {
-                    country: 'Russia',
-                    city: 'Moscow',
-                }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',            followed: false,
-                fullName: 'Anatoly',
-                status: 'Working',
-                location: {
-                    country: 'Russia',
-                    city: 'Naryan-Mar'
-                }
-            },
-        ])
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items)
+            });
     }
 
     return <div>{
-       props.users.map( u =>
-           <div key={u.id}>
+        props.users.map(u =>
+            <div key={u.id}>
               <span>
                   <div>
-                      <img src={u.photoUrl} className={styles.userPhoto}/>
+                      <img src={u.photos.small !== null
+                          ? u.photos.small
+                          : userPhoto} className={styles.userPhoto}/>
                   </div>
                   <div>
-                      { u.followed
-                          ? <button onClick={ () => { props.unfollow(u.id)}}>Unfollow</button>
-                          : <button onClick={ () => { props.follow(u.id)}}>Follow</button>}
+                      {u.followed
+                          ? <button onClick={() => {
+                              props.unfollow(u.id)
+                          }}>Unfollow</button>
+                          : <button onClick={() => {
+                              props.follow(u.id)
+                          }}>Follow</button>}
                   </div>
               </span>
-               <span>
+                <span>
                    <span>
-                       <div>{u.fullName}</div>
+                       <div>{u.name}</div>
                        <div>{u.status}</div>
                    </span>
                    <span>
-                       <div>{u.location.country}</div>
-                       <div>{u.location.city}</div>
+                       <div>{"u.location.country"}</div>
+                       <div>{"u.location.city"}</div>
                    </span>
                </span>
-           </div>
-       )
+            </div>
+        )
     }</div>
 }
 
 export default Users;
+
+/*props.setUsers([
+    {
+        id: 1,
+        photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',
+        followed: true,
+        fullName: 'Dmitry',
+        status: 'Learning',
+        location: {
+            country: 'Russia',
+            city: 'St. Petersburg',
+        }
+    },
+    {
+        id: 2,
+        photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',            followed: false,
+        fullName: 'Alexey',
+        status: 'Drinking',
+        location: {
+            country: 'Russia',
+            city: 'Moscow',
+        }
+    },
+    {
+        id: 3,
+        photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',            followed: false,
+        fullName: 'Anatoly',
+        status: 'Working',
+        location: {
+            country: 'Russia',
+            city: 'Naryan-Mar'
+        }
+    },
+])*/
