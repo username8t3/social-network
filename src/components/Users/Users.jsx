d5997ca9a7d059889a6c51ddf7b61f256ac6e462
Dmitry <56539475+username8t3@users.noreply.github.com>
2020-12-09 13:51:07 +0300

    :alien:

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,27 @@
 import React from "react";
-import styles from './users.module.css';
-import * as axios from 'axios';
-import userPhoto from '../../assets/images/user.png'
+import styles from "./users.module.css";
+import userPhoto from "../../assets/images/user.png";
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            });
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= 10; i++) { /* i <= pagesCount */
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            });
-    }
-
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-
-        return <div>
-            <div>
-                {pages.map(p => {
-                    return <span
-                        className={this.props.currentPage === p && styles.selectedPage}
-                        onClick={(e) => {this.onPageChanged(p);}}
-                    >{p}</span>
-                })}
-            </div>
-            {this.props.users.map(u => <div key={u.id}>
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span
+                    className={props.currentPage === p && styles.selectedPage}
+                    onClick={(e) => {
+                        props.onPageChanged(p);
+                    }}
+                >{p}</span>
+            })}
+        </div>
+        {props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small !== null
@@ -46,13 +29,15 @@ class Users extends React.Component {
                             : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>{u.followed
-                                ? <button onClick={() => {
-                                    this.props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={() => {
-                                    this.props.follow(u.id)}}>Follow</button>}
+                        ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+            <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -62,43 +47,8 @@ class Users extends React.Component {
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)}
-        </div>
-    }
+        </div>)}
+    </div>
 }
 
 export default Users;
-
-/*props.setUsers([
-    {
-        id: 1,
-        photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',
-        followed: true,
-        fullName: 'Dmitry',
-        status: 'Learning',
-        location: {
-            country: 'Russia',
-            city: 'St. Petersburg',
-        }
-    },
-    {
-        id: 2,
-        photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',            followed: false,
-        fullName: 'Alexey',
-        status: 'Drinking',
-        location: {
-            country: 'Russia',
-            city: 'Moscow',
-        }
-    },
-    {
-        id: 3,
-        photoUrl: 'https://www.f-legrand.fr/scidoc/figures/image/niveaux/images/babouin.png',            followed: false,
-        fullName: 'Anatoly',
-        status: 'Working',
-        location: {
-            country: 'Russia',
-            city: 'Naryan-Mar'
-        }
-    },
-])*/
