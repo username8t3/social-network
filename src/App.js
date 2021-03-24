0855d12864ce91e66d9d55d5b3ecf993da3f0ecd
Dmitry Shevelev <dsh@fv.ee>
2021-03-24 21:47:28 +0300

    61 - React JS - cookie, login в теории, auth/me

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 const App = (props) => {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
-                <Header/>
+                <HeaderContainer/>
                 <Navbar/>
                 <div className="app-wrapper-content">
                     <Route path="/dialogs" render={ () => <DialogsContainer /> } />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import css from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.css';
 
-const Header = () => {
-  return(
-    <header className={css.header}>
-      <img src="https://static1.squarespace.com/static/575a6067b654f9b902f452f4/59e64b57cf81e064f900819a/5c8c127beb393116ec4e1c52/1552683697079/300Logo.png?format=1500w"></img>
-    </header>
-  );
+const Header = (props) => {
+    // debugger;
+    return (
+        <header className={styles.header}>
+            <img src="https://static1.squarespace.com/static/575a6067b654f9b902f452f4/59e64b57cf81e064f900819a/5c8c127beb393116ec4e1c52/1552683697079/300Logo.png?format=1500w">
+            </img>
+            <div className={styles.loginBlock}>
+                { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+            </div>
+        </header>
+    );
 }
 
 export default Header;
